9b4a44a3bfb30965c9fe7b9341b34ba31c2b3655
huang丶shen <1426887150@qq.com>
2021-06-03 19:00:55 +0800

    Create 合并txt文件.py

---
+++ 杂记/合并txt文件.py
@@ -0,0 +1,52 @@
+import os
+
+def isFile(filePath,f):#批量读取文件
+    filename=filePath.split('\\')[-1]#拆分文件路径获得文件名
+    # 打印文件名
+    print("修改前文件名:"+filename)
+    split=os.path.splitext(filename)#拆分文件名和扩展名
+
+    # 如果文件名为result.txt,则清空里面的内容,作为结果文件
+    if filename == "result.txt":
+        f.truncate()
+
+    # 拓展名为txt,注意:不包含.py
+    if split[1] == ".txt" and split[1]!=".py" and filename != "result.txt":
+        print("文件路径:"+filePath)
+        for line in open(filePath,encoding='gbk', errors='ignore'):
+            # print(str(line))
+            f.writelines(line)
+        f.write('\n')
+
+def openDir(filePath,f):#递归文件夹
+    pathDir=os.listdir(filePath)#返回包含的文件或文件夹的名字的列表
+    for filename in pathDir:#遍历列表
+        childPath=os.path.join(filePath,filename)
+
+        a = os.path.isfile(childPath)
+
+        #判断是否为文件夹
+        if os.path.isfile(childPath):
+            isFile(childPath,f)
+        else:
+            openDir(childPath,f)
+
+
+# rootDir=r'D:\\files\文件夹'
+rootDir=os.getcwd()#根目录--工作目录
+
+pathDir =  os.listdir(rootDir)#列出根目录下所有内容
+
+f=open('result.txt','r+')
+
+
+for allDir in pathDir:#遍历列表
+    filepath=os.path.join(rootDir,allDir)#文件路径合成
+    aa = os.path.isfile(filepath)
+    #判断是否为文件夹
+    if os.path.isfile(filepath):
+        isFile(filepath,f)
+    else:
+        openDir(filepath,f)
+
+input("Enter Pass")
